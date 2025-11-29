7f3c7a3ea023c9b41c0f0543a084fbd9b28f5ec6
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-11-30 02:09:15 +0530

    refactor(components): change path for better ui

--- src/components/shapes/HollowCirclePattern.tsx
+++ src/components/shapes/HollowCirclePattern.tsx
@@ -1,24 +1,51 @@
 const HollowCirclePattern = () => {
   return (
     <div>
-      <svg viewBox="0 0 353 363" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_d_130_230)">
-<path d="M468.242 228.252C468.242 183.108 454.628 138.978 429.123 101.442C403.617 63.9064 367.364 34.6507 324.95 17.3749C282.535 0.0989808 235.864 -4.42115 190.837 4.38601C145.809 13.1932 104.449 34.9321 71.9868 66.8537C39.5241 98.7754 17.4168 139.446 8.46033 183.723C-0.496094 227.999 4.10068 273.893 21.6694 315.601C39.2381 357.308 68.9896 392.957 107.162 418.037C145.334 443.118 190.212 456.505 236.121 456.505L236.121 360.751C209.471 360.751 183.42 352.98 161.261 338.421C139.102 323.862 121.832 303.168 111.633 278.958C101.435 254.747 98.7665 228.105 103.966 202.403C109.165 176.701 121.998 153.092 140.842 134.562C159.687 116.031 183.696 103.412 209.834 98.2996C235.972 93.1871 263.064 95.811 287.686 105.84C312.307 115.868 333.351 132.851 348.157 154.64C362.963 176.429 370.866 202.047 370.866 228.252L468.242 228.252Z" fill="#756E2B"/>
-</g>
-<defs>
-<filter id="filter0_d_130_230" x="0" y="0" width="472.242" height="464.505" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="4"/>
-<feGaussianBlur stdDeviation="2"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_130_230"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_130_230" result="shape"/>
-</filter>
-</defs>
-</svg>
-
+      <svg viewBox="0 0 313 329" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_250_3)">
+          <path
+            d="M309 160.5C309 249.142 240.723 321 156.5 321C72.2766 321 4 249.142 4 160.5C4 71.8583 72.2766 0 156.5 0C240.723 0 309 71.8583 309 160.5ZM72.8684 160.5C72.8684 209.111 110.312 248.519 156.5 248.519C202.688 248.519 240.132 209.111 240.132 160.5C240.132 111.889 202.688 72.4812 156.5 72.4812C110.312 72.4812 72.8684 111.889 72.8684 160.5Z"
+            fill="#756E2B"
+          />
+        </g>
+        <defs>
+          <filter
+            id="filter0_d_250_3"
+            x="0"
+            y="0"
+            width="313"
+            height="329"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="2" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_250_3"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_250_3"
+              result="shape"
+            />
+          </filter>
+        </defs>
+      </svg>
     </div>
   );
 };
