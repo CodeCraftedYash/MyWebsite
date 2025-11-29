0f34bd81ebee283ec06d49ad6f07c2e0adc4e526
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-11-29 23:53:52 +0530

    style(components):implement css variable for font size

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -8,8 +8,8 @@ type BtnProps = {
 
 const Button = ({cb,Icon,text}:BtnProps) => {
   return (
-    <button onClick={cb} className="bg-gray-300 p-2 px-4 rounded-(--border-radius) hover:scale-110 duration-200 ease-in-out text-white font-semibold text-sm text-nowrap flex flex-nowrap items-center justify-between gap-2"
-    style={{backgroundImage:"var(--gradient-blue)",boxShadow:"var(--box-shadow)"}}>{text} <Icon /></button>
+    <button onClick={cb} className="bg-gray-300 p-2 px-4 rounded-(--border-radius) hover:scale-110 duration-200 ease-in-out text-white font-semibold text-nowrap flex flex-nowrap items-center justify-between gap-2"
+    style={{backgroundImage:"var(--gradient-blue)",boxShadow:"var(--box-shadow)",fontSize:"var(--font-size-base)"}}>{text} <Icon /></button>
   )
 }
 
