2f37d021c37ef714f9d318250792287a64d85c29
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-01 23:57:04 +0530

    feat(layout): add a component for front heading

---
+++ src/layouts/hero/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+        <h1 className="text-[8vw] sm:text-[6vw] md:text-[4.5vw] lg:text-[4vw]">Frontend</h1>
+        <h1 className="text-[8vw] sm:text-[6vw] md:text-[4.5vw] lg:text-[4vw]"> Developer</h1>
+        <h3 className="mt-1">React, TypeScript & Modern UI • Learning Node & Express</h3>
+    </div>
+  )
+}
+
+export default Header
