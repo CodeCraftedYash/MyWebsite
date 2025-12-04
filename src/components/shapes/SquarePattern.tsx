46d0d754e0308fecae7491ae8dd91d6cb210df20
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-04 23:35:38 +0530

    feat(components): add  movement animation using gsap

--- src/components/shapes/SquarePattern.tsx
+++ src/components/shapes/SquarePattern.tsx
@@ -1,16 +1,22 @@
+import { useRef } from "react";
 import BasicShapes from "./BasicShapes";
+import useMotionAnimationHook from "../../hooks/useMotionAnimationHook";
 
 const SquarePattern = () => {
+  const ref = useRef<HTMLDivElement|null>(null);
+  useMotionAnimationHook(ref,"data-anim", 25, 3,5,1,true,2,3);
   return (
-    <div className="aspect-square flex flex-col flex-nowrap origin-center">
-      <div className="w-[15%] ml-[60%]">
+    <div className="aspect-square flex flex-col flex-nowrap origin-center" ref={ref}>
+      <div className="w-[15%] ml-[60%]" data-anim>
         <BasicShapes type="square" />
       </div>
       <div className="flex flex-nowrap mt-auto">
-        <div className="w-[70%]"><BasicShapes type="square" /></div>
-        <BasicShapes type="square" />
-        <div className="w-[15%] ml-auto -mt-[5%]"><BasicShapes type="square" /></div>
-        <BasicShapes type="square" />
+        <div className="w-[70%]" data-anim>
+          <BasicShapes type="square" />
+        </div>
+        <div className="w-[15%] ml-auto -mt-[5%]" data-anim>
+          <BasicShapes type="square" data-anim/>
+        </div>
       </div>
     </div>
   );
