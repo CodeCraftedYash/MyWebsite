6e486a6c438f7443f098ac4a41f79071963162da
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-05 11:59:44 +0530

    refactor(hooks): add more parameters and make the hook dynamic

--- src/hooks/useMotionAnimationHook.tsx
+++ src/hooks/useMotionAnimationHook.tsx
@@ -2,20 +2,34 @@ import React from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
-export default function useMotionAnimationHook(container: React.RefObject<HTMLElement[]>,rotation:number,movement:number){
-    useGSAP(()=>{
-        container.current.forEach(el=>{
-            if(!el)
-            return;
-        gsap.to(el,{
-            x:()=>gsap.utils.random(-movement,1,movement),
-            y:()=>gsap.utils.random(-movement,1,movement),
-            rotate:()=>gsap.utils.random(-rotation,5,rotation),
-            repeat:-1,
-            repeatRefresh:true,
-            duration:()=>gsap.utils.random(2,3)
-        })
-        })
-        
-    },[container])
+export default function useMotionAnimationHook(
+  container: React.RefObject<HTMLElement | null>,
+  selector: string,
+  rotation: number,
+  movement: number,
+  rSnap: number,
+  mSnap: number,
+  repeat: boolean,
+  durationMin: number,
+  durationMax: number
+) {
+  useGSAP(
+    (context) => {
+      if (!container.current) return;
+      const select = context.selector!;
+      const elements = select(selector);
+      if (!elements || !elements.length) return;
+      elements.forEach((el: HTMLDivElement) => {
+        gsap.to(el, {
+          x: () => gsap.utils.random(-movement, movement, mSnap),
+          y: () => gsap.utils.random(-movement, movement, mSnap),
+          rotate: () => gsap.utils.random(-rotation, rotation, rSnap),
+          repeat: repeat ? -1 : 0,
+          repeatRefresh: true,
+          duration: () => gsap.utils.random(durationMin, durationMax),
+        });
+      });
+    },
+    { scope: container }
+  );
 }
