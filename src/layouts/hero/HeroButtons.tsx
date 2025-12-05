3e1b4b5ce1eaac620ebe8ca145274e4a161fa4dc
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-06 03:11:24 +0530

    feat(layouts):add animation using custom hook and gsap

--- src/layouts/hero/HeroButtons.tsx
+++ src/layouts/hero/HeroButtons.tsx
@@ -1,12 +1,18 @@
 import Button from "../../components/buttons/Button"
 import { FaFileDownload } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
+import { useFadeInAnimation } from "../../hooks/useFadeInAnimation";
+import { useRef } from "react";
 
 const HeroButtons = () => {
+  const ref1 = useRef<HTMLDivElement>(null);
+  const ref2 = useRef<HTMLDivElement>(null);
+  useFadeInAnimation(ref1,0.5,0.1,0,5);
+  useFadeInAnimation(ref2,0.5,0.1,0,5);
   return (
     <div className="flex flex-nowrap justify-between gap-5">
-        <Button cb={()=>alert("button was clicked")} text="Hire Me" Icon={FaArrowRight}/>
-        <Button cb={()=>alert("button was clicked")} text="CV" Icon={FaFileDownload}/>
+        <div ref={ref1}><Button cb={()=>alert("button was clicked")} text="Hire Me" Icon={FaArrowRight}/></div>
+        <div ref={ref2}><Button cb={()=>alert("button was clicked")} text="CV" Icon={FaFileDownload}/></div>
     </div>
   )
 }
