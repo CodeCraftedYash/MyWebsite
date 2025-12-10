07f5d0307aeea6025469069059ac9eae5cbaff32
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-10 23:53:08 +0530

    style(components): change the gap from percentage to px

--- src/components/shapes/BeakPattern.tsx
+++ src/components/shapes/BeakPattern.tsx
@@ -2,7 +2,7 @@ import BasicShapes from "./BasicShapes"
 
 const BeakPattern = () => {
   return (
-    <div className="flex flex-nowrap gap-[5%] origin-center ">
+    <div className="flex flex-nowrap gap-5 origin-center ">
         <div className="w-full aspect-square"><BasicShapes type="circle"/></div>
         <div className=" w-full rotate-90"><BasicShapes type="triangle"/></div>
     </div>
