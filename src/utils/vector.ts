d67277f615bbb7f2977a017550f45e6e279023b1
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-15 00:00:29 +0530

    feat(utils): add a utility function to convert degrees into vector x and y value

---
+++ src/utils/vector.ts
@@ -0,0 +1,14 @@
+/**
+ * this will take a degree in number from the user
+ * make a boundry of 360 degree so the rotation is wrapped up
+ * convert this degree into radian using radians = degrees * (pi/100)
+ * Variables : x gets SINE of the rotation and y gets the COS of the rotation
+ * return x and y
+ */
+export const Vectorize = (rotation:number) => {
+    const r = rotation % 360;
+    const radians = r * (Math.PI / 100);
+    const x:number = Math.cos(radians);
+    const y:number = Math.sin(radians);
+    return {x,y};
+}
