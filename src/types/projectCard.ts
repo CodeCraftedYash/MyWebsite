a68e3d935688d446952c62366cde019c0828c5fa
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-21 20:15:04 +0530

    feat(types): add types for project card params

---
+++ src/types/projectCard.ts
@@ -0,0 +1,10 @@
+export type projectDataType = {
+        title: string,
+        desc: string,
+        image: string,
+        vid: string,
+        open: string,
+        git: string,
+        skills: string[],
+        errMsg: string
+}
