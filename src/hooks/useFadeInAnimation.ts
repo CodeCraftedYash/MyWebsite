cd5b78b9f4d3b89346b057e1185198a5cfa59813
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-24 23:06:12 +0530

    refactor(hooks): add a boolean param to control the execution of inner contents

--- src/hooks/useFadeInAnimation.ts
+++ src/hooks/useFadeInAnimation.ts
@@ -5,13 +5,13 @@ import type { fadeInHookType } from "../types/fadeInHookType";
 export const useFadeInAnimation : fadeInHookType = (
   ref,
   duration,
+  gate = false,
   delay,
   x,
   y,
 ) => {
   useGSAP(() => {
-    if (!ref.current) return;
-
+    if (!ref.current || !gate) return;
     gsap.set(ref.current, { autoAlpha: 0,x,y });
     gsap.to(ref.current, {
       autoAlpha: 1,
@@ -21,5 +21,5 @@ export const useFadeInAnimation : fadeInHookType = (
       x:0,
       y:0
     });
-  },[ref,duration,delay,x,y]);
+  },[gate,ref,duration,delay,x,y]);
 };
