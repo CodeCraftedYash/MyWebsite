200837529c8f6bb5777f90bd3c5f9b1cdb4e2ba6
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-24 23:12:47 +0530

    refactor(sections): update hooks and refs implementation

--- src/sections/aboutSection/AboutSection.tsx
+++ src/sections/aboutSection/AboutSection.tsx
@@ -1,36 +1,30 @@
 import { useRef, useState } from "react";
 import Header from "../../components/header/Header";
-import { useFadeInAnimation } from "../../hooks/useFadeInAnimation";
 import { useScrollTrigger } from "../../hooks/useScrollTrigger";
 import AboutMain from "../../layouts/about/AboutMain";
 import DesignAbout from "../../layouts/about/DesignAbout";
 
 const AboutSection = () => {
-  const ref = useRef<HTMLDivElement>(null);
   const refContainer = useRef<HTMLDivElement>(null);
   const [view, setView] = useState<boolean>(false);
-  useFadeInAnimation(ref, 0.5, 1, 0, 0);
-  useScrollTrigger(ref, "top 70%", "bottom 100%", true, false, false, () =>
-    setView(true)
-  );
+  useScrollTrigger(refContainer,'top center','bottom bottom',true,false,false,()=>setView(true))
+
   return (
     <div
-      className="min-h-screen bg-(--color-primary) bg-linear-120 from-(--color-primary) to-(--color-secondary) relative overflow-hidden"
-      ref={ref}
+      className="min-h-dvh bg-(--color-primary) bg-linear-120 from-(--color-primary) to-(--color-secondary) relative overflow-hidden"
+      ref={refContainer}
+      id="about"
     >
-      {view && (
         <div
-          ref={refContainer}
           className="flex flex-col gap-20 pt-10 relative overflow-hidden"
         >
-          <div className="relative z-50">
+           {view && <div className="relative z-50">
             <Header text="About Me" />
-          </div>
-          <div className="relative z-50">
+          </div>}
+          {view && <div className="relative z-50">
             <AboutMain />
-          </div>
+          </div>}
         </div>
-      )}
       <DesignAbout />
     </div>
   );
