b4c6f35c763a33e549ef17fd06ebab67466fb0ff
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-26 03:04:17 +0530

    chore(constants): add an index file to simplify import off svgs

---
+++ src/constants/Skills/index.ts
@@ -0,0 +1,17 @@
+import reactIcon from '../../assets/reactIcon.svg';
+import nextjsIcon from '../../assets/nextjs.svg';
+import typescriptIcon from '../../assets/typescript.svg';
+import tailwindIcon from '../../assets/tailwind.svg';
+import gsapIcon from '../../assets/gsap.svg';
+import nodejsIcon from '../../assets/nodejs.svg';
+import expressIcon from '../../assets/express.svg';
+import restApiIcon from '../../assets/restApi.svg';
+import mongoDBIcon from '../../assets/mongodb.svg';
+import mongooseIcon from '../../assets/mongoose.svg';
+import gitIcon from '../../assets/git.svg';
+import githubIcon from '../../assets/github.svg';
+import postmanIcon from '../../assets/postman.svg';
+import htmlIcon from '../../assets/html.svg';
+import cssIcon from '../../assets/css.svg';
+import javascriptIcon from '../../assets/javascript.svg';
+export { reactIcon ,nextjsIcon ,typescriptIcon ,tailwindIcon ,gsapIcon ,nodejsIcon ,expressIcon ,restApiIcon ,mongoDBIcon ,mongooseIcon ,gitIcon ,githubIcon ,postmanIcon, htmlIcon, cssIcon, javascriptIcon }
