f9c1c4a3655eb5ecd7144679ee19632d745c4eea
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-29 22:51:55 +0530

    feat(layout): add navbar layout component

---
+++ src/layouts/navbar/Navbar.tsx
@@ -0,0 +1,45 @@
+import { useRef, useState } from "react";
+import { navItems } from "../../constants/Nav/nav";
+import { useSectionTracker } from "../../hooks/useSectionTracker";
+import { scrollIntoView } from "../../utils/scrollIntoView";
+import HamburgerMenu from "./HamburgerMenu";
+import { useAutoHideNav } from "../../hooks/useAutoHideNav";
+
+const Navbar = () => {
+  const HeaderRef = useRef(null);
+  const [active, setActive] = useState("home");
+  useSectionTracker(setActive);
+  useAutoHideNav(HeaderRef);
+  return (
+    <header className="fixed w-full h-16 left-0 top-0 z-100 flex  justify-between px-8 items-center gap-5 text-(--color-text-dark)" ref={HeaderRef}>
+      <div
+        className="logo hover:cursor-pointer whitespace-nowrap"
+        onClick={()=>{
+          scrollIntoView("#home");
+        }}
+      >
+        Yash Mishra
+      </div>
+      <nav className="hidden md:flex gap-5 ">
+        {navItems.map((el, index) => (
+          <div
+            className="flex flex-nowrap items-center gap-1 font-bold hover:cursor-pointer hover:scale-120 transition-all duration-200 ease-in-out "
+            key={index}
+            onClick={() => {
+              scrollIntoView(el.to);
+            }}
+            style={{
+              color : active == el.text.toLowerCase() ? "var(--color-accent)" : "var(--color-text-dark)"
+            }}
+          >
+            <el.icons />
+            <h3>{el.text}</h3>
+          </div>
+        ))}
+      </nav>
+      <div className="md:hidden"><HamburgerMenu active={active} /></div>
+    </header>
+  );
+};
+
+export default Navbar;
