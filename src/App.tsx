90cdbfc62a8769e060c6cb9dc295f6f699c7fdd2
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-31 05:51:50 +0530

    feat(theme): apply persisted dark theme on app initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import HomePage from "./pages/HomePage"
+import { getTheme } from "./utils/localStorageUtil"
 
 function App() {
+    const theme = getTheme();
+    if(theme === 'dark')
+        document.documentElement.setAttribute('dark-theme','dark');
  return(
   <>
   <HomePage />
