f9b4ac595c6d4ac112a982f44e286c9c9313162b
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-12-31 06:03:16 +0530

    feat(types): add gate boolean item in params

--- src/types/fadeInHookType.ts
+++ src/types/fadeInHookType.ts
@@ -3,6 +3,8 @@ import type { RefObject } from "react";
 export type fadeInHookType = (
     ref: RefObject<HTMLElement | HTMLDivElement | null>,
       duration: number,
+      gate:boolean,
       delay?: number,
       x?: number,
-      y?: number,) => void
+      y?: number,    
+    ) => void
