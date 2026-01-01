666648775394ee2e9f01fa70a8522ac564bf0012
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-01-01 20:58:34 +0530

    feat(sections): add skill section

---
+++ src/sections/skillsSection/SkillsSection.tsx
@@ -0,0 +1,20 @@
+import { useRef, useState } from "react"
+import Header from "../../components/header/Header"
+import SkillsLayout from "../../layouts/skill/SkillsLayout";
+import { useScrollTrigger } from "../../hooks/useScrollTrigger";
+import DesignSkill from "../../layouts/skill/DesignSkill";
+
+const SkillsSection = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [ active, setActive ] = useState<boolean>(false);
+  useScrollTrigger(ref,"top 80%","top 60%",true,false,false,()=>setActive(true));
+  return (
+    <div className="min-h-dvh py-10 flex flex-col gap-10 z-50 relative bg-(--color-background)" id="skills" >
+      <div className="text-(--color-text-dark)">{active && <Header text="Skills"/>}</div>
+      <div className="w-full md:w-[80%] mx-auto" ref={ref}><SkillsLayout /></div>
+      <DesignSkill />
+    </div>
+  )
+}
+
+export default SkillsSection
