dc1b9056402b346be817f9d670c98bda8c42d1ec
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-01-01 18:36:59 +0530

    fix(layouts): add fetchPriority high for better loading speed

--- src/layouts/hero/Profile.tsx
+++ src/layouts/hero/Profile.tsx
@@ -4,10 +4,10 @@ import LineSvg from "../../components/svgs/LineSvg";
 import { useFadeInAnimation } from "../../hooks/useFadeInAnimation";
 const Profile = () => {
   const ref = useRef<HTMLImageElement>(null);
-  useFadeInAnimation(ref,0.5,0.3)
+  useFadeInAnimation(ref,0.5,true,0.3);
   return (
     <div className="relative w-fit">
-      <img src={MyImage} ref={ref} className="w-44 min-w-10 aspect-square rounded-full" style={{boxShadow:"var(--box-shadow)"}}/>
+      <img src={MyImage} ref={ref} fetchPriority="high" className="w-44 min-w-10 aspect-square rounded-full" style={{boxShadow:"var(--box-shadow)"}}/>
       <div className="absolute w-full h-full -left-8 -bottom-4"><LineSvg /></div>
     </div>
   );
