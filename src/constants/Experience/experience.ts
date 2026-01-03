b11873e3a3c24f87a0c74f85f41bd3bbbf7fe754
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-01-03 23:58:47 +0530

    feat(constants): add data for experience section

---
+++ src/constants/Experience/experience.ts
@@ -0,0 +1,19 @@
+type experienceData = {
+    cName : string;
+    position : string;
+    period : string;
+    accomplishments: string[];
+    img: string;
+    cLink: string;
+}
+import branduckLogo from '../../assets/branduckLogo.jpg';
+export const experienceData:experienceData[] = [
+    {
+    cName:"Branduck",
+    position:"WebDeveloper Intern",
+    period:"Oct 2023 - Jan 2024",
+    accomplishments : ["Worked as part of the development team building web-based applications using Oracle APEX","Designed and implemented custom UI components using CSS to enhance layout, responsiveness, and visual consistency.","Wrote JavaScript scripts to add interactivity and improve user experience within Oracle APEX applications.","Created and managed SQL tables and queries to support application data requirements.","Gained hands-on experience working with low-code platforms integrated with custom frontend logic."],
+    img:branduckLogo ,
+    cLink:"https://www.linkedin.com/company/branduck/posts/?feedView=all"
+    },
+]
