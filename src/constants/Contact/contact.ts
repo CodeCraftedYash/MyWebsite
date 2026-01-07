d8d7491047e9606d61ae4e0667d2003582e8b18a
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-01-08 04:21:14 +0530

    feat(constants): add data for social media used in contact section

---
+++ src/constants/Contact/contact.ts
@@ -0,0 +1,9 @@
+import linkedInLogo from '../../assets/linkedinlogo.svg';
+import instagramLogo from '../../assets/instaLogo.svg';
+import githubLogo from '../../assets/github.svg';
+
+export const contact = [
+    {text:"Instagram",icon:instagramLogo,link:"https://www.instagram.com/yash_coding/"},
+    {text:"Github",icon:githubLogo,link:"https://github.com/CodeCraftedYash"},
+    {text:"LinkedIn",icon:linkedInLogo,link:"https://www.linkedin.com/in/yash-mishra1000/"},
+]
