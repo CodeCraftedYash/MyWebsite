37be5eb0e02b1e31f17689cf362b6fe1dff74caa
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-01-09 03:23:02 +0530

    style(sections): increase padding for better ui

--- src/sections/skillsSection/SkillsSection.tsx
+++ src/sections/skillsSection/SkillsSection.tsx
@@ -9,7 +9,7 @@ const SkillsSection = () => {
   const [ active, setActive ] = useState<boolean>(false);
   useScrollTrigger(ref,"top 80%","top 60%",true,false,false,()=>setActive(true));
   return (
-    <div className="min-h-dvh py-10 flex flex-col gap-10 z-50 relative bg-(--color-background)" id="skills" >
+    <div className="min-h-dvh py-20 flex flex-col gap-10 z-50 relative bg-(--color-background)" id="skills" >
       <div className="text-(--color-text-dark)">{active && <Header text="Skills"/>}</div>
       <div className="w-full md:w-[80%] mx-auto" ref={ref}><SkillsLayout /></div>
       <DesignSkill />
