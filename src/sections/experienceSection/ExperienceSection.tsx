bc7c3cb8f59833318e33dc667dfc3bb4ac396ede
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-01-09 02:38:17 +0530

    refactor(sections): split section code into a layout component

--- src/sections/experienceSection/ExperienceSection.tsx
+++ src/sections/experienceSection/ExperienceSection.tsx
@@ -1,32 +1,28 @@
-import { useRef, useState } from "react"
-import Header from "../../components/header/Header"
+import { useRef, useState } from "react";
+import Header from "../../components/header/Header";
 import { useScrollTrigger } from "../../hooks/useScrollTrigger";
-import ExperienceCard from "../../components/cards/ExperienceCard";
-import { experienceData } from "../../constants/Experience/experience";
-import Shine from "../../components/overlayEffects/Shine";
-import { useCardsSlideIn } from "../../hooks/useCardsSlideIn";
+import ExperienceMain from "../../layouts/experience/ExperienceMain";
+import DesignExperience from "../../layouts/experience/DesignExperience";
 
 const ExperienceSection = () => {
   const ref = useRef<HTMLDivElement>(null);
-  const cardsRef = useRef<HTMLDivElement>(null);
-  const [ active, setActive ] = useState<boolean>(false);
-  useScrollTrigger(ref,"top 80%","top 60%",true,false,false,()=>setActive(true));
-  useCardsSlideIn(cardsRef);
-  const data = experienceData;
+  const [active, setActive] = useState<boolean>(false);
+  useScrollTrigger(ref, "top 80%", "top 60%", true, false, false, () =>
+    setActive(true)
+  );
   return (
-    <div className="min-h-dvh py-20 flex flex-col gap-10 z-50 relative bg-(--color-background)" id="experience" ref={ref}>
-      <div className="text-(--color-text-dark) mb-12">{active && <Header text="Experience"/>}</div>
-      <div className="w-full md:w-[80%] mx-auto">
-        <div className="w-[80%] mx-auto flex flex-col gap-20" ref={cardsRef}>{
-          data.map(( item, index ) => (
-            <div key = {index} className="h-full">
-              <Shine color="#ffffff66" direction={0} duration={1.5} rotation={45} type="enter" width="7%"><ExperienceCard data = {item} /></Shine>
-              </div>
-          ))
-        }</div>
+    <div
+      className="min-h-dvh py-20 flex flex-col gap-10 relative bg-(--color-background) z-50"
+      id="experience"
+      ref={ref}
+    >
+      <div className="text-(--color-text-dark) mb-12 z-50">
+        {active && <Header text="Experience" />}
       </div>
+      <ExperienceMain />
+      <DesignExperience />
     </div>
-  )
-}
+  );
+};
 
-export default ExperienceSection
+export default ExperienceSection;
