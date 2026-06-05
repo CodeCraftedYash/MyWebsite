d2e04175f3a56e8921baf4bed2552938136729c1
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-06-06 02:00:04 +0530

    refactor(sections): make items flex after two per row

--- src/sections/projectSection/ProjectSection.tsx
+++ src/sections/projectSection/ProjectSection.tsx
@@ -12,7 +12,7 @@ const ProjectSection = () => {
   const refCard = useRef<HTMLDivElement>(null)
   const [active,setActive] = useState(false);
   const projectCards = projectsData.map((item)=>(
-    <div key={item.title} className=" md:w-1/3 row rounded-(--border-radius)">
+    <div key={item.title} className=" md:w-1/3 rounded-(--border-radius)">
              <div className="hover:scale-105 transition-all duration-200 rounded-(--border-radius) h-full" style={{boxShadow:"var(--box-shadow)"}}><Shine  color="#ffffff66" direction={0} duration={1.5} rotation={45} type="enter" width="10%" > <ProjectCard title={item.title} desc={item.desc} errMsg={item.errMsg} image={item.image} skills={item.skills} git={item.git} open={item.open} vid={item.vid} /> </Shine>
              </div>
             </div>
@@ -23,7 +23,7 @@ const ProjectSection = () => {
     <div id="projects" className="min-h-screen bg-(--color-background)  relative z-50" ref={ref}>
        <div className="flex flex-col gap-20 py-10 text-(--color-text-dark)">
       {active && <Header text="My Works" />}
-      <div className="flex flex-col md:flex-row gap-20 w-[80%] md:w-[95%] mx-auto mt-5 md:mt-0 justify-center opacity-0" ref={refCard}>
+      <div className="flex flex-col md:flex-row flex-wrap gap-16 w-[80%] md:w-[95%] mx-auto mt-5 md:mt-0 justify-center opacity-0 " ref={refCard}>
         {projectCards}
       </div>
       </div>
